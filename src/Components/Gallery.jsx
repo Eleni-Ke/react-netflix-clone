567163d1e5b97164877775324ed3c5c353b9552e
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-02-05 21:02:47 +0100

    fetch moved to Gallery, usable for any saga now

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,13 +1,36 @@
+import { Component } from "react";
 import Movie from "./Movie";
 
-const Gallery = (props) => {
-  return (
-    <>
-      {props.sagaArr.map((movie) => {
-        return <Movie singleMovie={movie} key={movie.imdbID} />;
-      })}
-    </>
-  );
-};
+const url = "http://www.omdbapi.com/?apikey=7a852a40&s=";
+
+class Gallery extends Component {
+  state = {
+    sagaArr: [],
+  };
+  getSaga = async () => {
+    try {
+      let response = await fetch(url + this.props.saga);
+      let movieArr = await response.json();
+      movieArr = movieArr.Search;
+      this.setState({ sagaArr: movieArr });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  componentDidMount() {
+    this.getSaga();
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.sagaArr.map((movie) => {
+          return <Movie singleMovie={movie} key={movie.imdbID} />;
+        })}
+      </>
+    );
+  }
+}
 
 export default Gallery;
--- src/Components/MainSection.jsx
+++ src/Components/MainSection.jsx
@@ -3,108 +3,25 @@
 import { Component } from "react";
 import { Container, Row } from "react-bootstrap";
 import Gallery from "./Gallery";
-// const array = [
-//   {
-//     Title: "Harry Potter and the Deathly Hallows: Part 2",
-//     Year: "2011",
-//     imdbID: "tt1201607",
-//     Type: "movie",
-//     Poster: "https://bit.ly/3sufYok",
-//   },
-//   {
-//     Title: "Harry Potter and the Sorcerer's Stone",
-//     Year: "2001",
-//     imdbID: "tt0241527",
-//     Type: "movie",
-//     Poster: "https://bit.ly/3tTtx0H",
-//   },
-//   {
-//     Title: "Harry Potter and the Chamber of Secrets",
-//     Year: "2002",
-//     imdbID: "tt0295297",
-//     Type: "movie",
-//     Poster: "https://bit.ly/31gVxzb",
-//   },
-//   {
-//     Title: "Harry Potter and the Prisoner of Azkaban",
-//     Year: "2004",
-//     imdbID: "tt0304141",
-//     Type: "movie",
-//     Poster: "https://bit.ly/2QzHt1n",
-//   },
-// ];
-
-const url = "http://www.omdbapi.com/?apikey=7a852a40&s=";
 
 class MainSection extends Component {
-  state = {
-    arrays: {
-      saga: [],
-      secondSaga: [],
-      thirdSaga: [],
-    },
-  };
-  getSaga = async () => {
-    try {
-      let response = await fetch(url + "Lord of the Rings");
-      let movieArr = await response.json();
-      movieArr = movieArr.Search;
-      this.setState({
-        arrays: {
-          ...this.state.arrays,
-          saga: movieArr,
-        },
-      });
-      let responseTwo = await fetch(url + "Harry Potter");
-      let secondMovieArr = await responseTwo.json();
-      secondMovieArr = secondMovieArr.Search;
-      this.setState({
-        arrays: {
-          ...this.state.arrays,
-          secondSaga: secondMovieArr,
-        },
-      });
-      let thirdResponse = await fetch(url + "Star Wars");
-      let thirdMovieArr = await thirdResponse.json();
-      thirdMovieArr = thirdMovieArr.Search;
-      this.setState({
-        arrays: {
-          ...this.state.arrays,
-          thirdSaga: thirdMovieArr,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  componentDidMount() {
-    this.getSaga();
-  }
   render() {
     return (
       <Container className="d-flex main-container">
         <>
           <h2>Lord of The Rings Saga:</h2>
           <Row>
-            <Gallery sagaArr={this.state.arrays.saga} />
+            <Gallery saga="Lord of the Rings" />
           </Row>
           <h2>Harry Potter Saga:</h2>
           <Row>
-            <Gallery sagaArr={this.state.arrays.secondSaga} />
+            <Gallery saga="Harry Potter" />
           </Row>
           <h2>Star Wars Saga:</h2>
           <Row>
-            <Gallery sagaArr={this.state.arrays.thirdSaga} />
+            <Gallery saga="Star Wars" />
           </Row>
         </>
-
-        {/* <Row>
-          <h2>The Star Wars Sage:</h2>
-        </Row>
-        <Row>
-          <h2>The Hunger Games Saga:</h2>
-        </Row> */}
       </Container>
     );
   }
